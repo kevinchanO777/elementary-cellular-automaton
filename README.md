3e5f6482d9d1c909d2dc6235ebf4ba9a07c64e4d
kevinchanO777 <kevinchan714456@gmail.com>
2023-08-28 15:52:35 +0800

    finished

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # elementary-cellular-automaton
-Use python to write a simulation of  elementary cellular automaton
+Use python to write a simulation of elementary cellular automaton
 
 ## Reference
 https://en.wikipedia.org/wiki/Elementary_cellular_automaton
---
+++ main.py
@@ -0,0 +1,46 @@
+def generate_rule_pattern(rule):
+    # Convert rule number to binary and reverse the order
+    rule_binary = format(rule,"08b")[::-1]
+    pattern = {key: rule_binary[key] for key in range(8)}
+    for key in pattern:
+        pattern[key] = '.' if pattern[key] == '0' else 'x'
+    return pattern
+
+def int_represent(pattern):
+    pattern = ''.join(['0' if item == '.' else '1' for item in pattern])
+    pattern = int(pattern,2)
+    return pattern
+
+def generate_elementary_ca(rule, num_steps):
+
+    rule_pattern = generate_rule_pattern(rule)
+ 
+    # Initial state with a single 'x' in the middle
+    state = ['.'] * (2 * num_steps + 1)
+    state[num_steps] = 'x'
+
+    # Print the initial state
+    print(''.join(state))
+
+    # Generate the subsequent states
+    for i in range(num_steps):
+        new_state = ['.'] * (2 * num_steps + 1)
+
+        new_state[0] = rule_pattern[int_represent([state[-1],state[0],state[1]])]
+        for i in range(1,len(state)-1):
+           new_state[i] = rule_pattern[int_represent(state[i-1:i+2])]
+        new_state[-1] = rule_pattern[int_represent([state[-2],state[-1],state[0]])]
+
+        state = new_state
+        print(''.join(state))
+
+# Example usage
+# Rule 18: give a fractal like pattern
+# Rule 30: interesting triangles too ...
+
+steps = 15
+
+for i in range(256):
+    print(f"Rule {i}: ")
+    generate_elementary_ca(i, steps)
+    print("\n")
